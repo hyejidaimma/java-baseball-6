d6c1e7b417e5306501f5cf9d691a856c0af5294b
Hyeji <hyejijjang10@gmail.com>
2023-11-24 15:33:55 +0900

    feat : 컴퓨터 패 검증 구현

---
+++ src/main/java/baseball/Model/RandomNumberGenerator.java
@@ -0,0 +1,18 @@
+package baseball.Model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
+public class RandomNumberGenerator {
+    public ArrayList<Integer> computerInit(){
+        ArrayList<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+}
